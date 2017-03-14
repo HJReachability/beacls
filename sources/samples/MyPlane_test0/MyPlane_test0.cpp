f4b0de17ba2e5c929453e5ab6c16e18ffc9d501e
Ken Tanabe <kenn.tanabe@toshiba.com>
2017-03-14 08:56:42 -0700

    - Updated tutorial.

--- sources/samples/MyPlane_test0/MyPlane_test0.cpp
+++ sources/samples/MyPlane_test0/MyPlane_test0.cpp
@@ -26,102 +26,50 @@ int main(int argc, char *argv[])
 		enable_user_defined_dynamics_on_gpu = (atoi(argv[8]) == 0) ? false : true;
 	}
 	//!< Plane parameters
-	const beacls::FloatVec initState{ (FLOAT_TYPE)100, (FLOAT_TYPE)75, (FLOAT_TYPE)(220 * M_PI / 180) };
-	const FLOAT_TYPE wMax = (FLOAT_TYPE)1.2;
-	const beacls::FloatVec vrange{ (FLOAT_TYPE)1.1, (FLOAT_TYPE)1.3 };
-	const beacls::FloatVec dMax{ (FLOAT_TYPE)0, (FLOAT_TYPE)0 };
-	Plane* pl = new Plane(initState, wMax, vrange, dMax);
+	/* 
+	To Be filled 
+	*/
 
 	const FLOAT_TYPE inf = std::numeric_limits<FLOAT_TYPE>::infinity();
 	//!< Target and obstacle
-	HJI_Grid* g = createGrid(
-		beacls::FloatVec{(FLOAT_TYPE)0, (FLOAT_TYPE)0, (FLOAT_TYPE)0}, 
-		beacls::FloatVec{(FLOAT_TYPE)150, (FLOAT_TYPE)150, (FLOAT_TYPE)(2*M_PI)}, 
-		beacls::IntegerVec{41,41,11});
-	std::vector<beacls::FloatVec > targets(1);
-	BasicShape* shapeTarget = new ShapeCylinder(beacls::IntegerVec{2}, beacls::FloatVec{75., 50., 0.}, (FLOAT_TYPE)10);
-	shapeTarget->execute(g, targets[0]);
-	BasicShape* shapeObs1 = new ShapeRectangleByCorner(beacls::FloatVec{300, 250, -inf}, beacls::FloatVec{350, 300, inf});
-	BasicShape* shapeObs2 = new ShapeRectangleByCorner(beacls::FloatVec{5, 5, -inf}, beacls::FloatVec{145, 145, inf});
-	beacls::FloatVec obs1, obs2;
-	shapeObs1->execute(g, obs1);
-	shapeObs2->execute(g, obs2);
-	std::transform(obs2.cbegin(), obs2.cend(), obs2.begin(), std::negate<FLOAT_TYPE>());
-	beacls::FloatVec obstacle(obs1.size());
-	std::transform(obs1.cbegin(), obs1.cend(), obs2.cbegin(), obstacle.begin(), 
-		std::ptr_fun<const FLOAT_TYPE&, const FLOAT_TYPE&>(std::min<FLOAT_TYPE>));
-	std::vector<const beacls::FloatVec* > obstacles(1);
-	obstacles[0] = &obstacle;
+	/* 
+	To Be filled 
+	*/
 
 	//!< Compute reachable set
-	const FLOAT_TYPE tMax = 500;
-	const FLOAT_TYPE dt = 0.25;
-	beacls::FloatVec tau = generateArithmeticSequence<FLOAT_TYPE>(0., dt, tMax);
+	/* 
+	To Be filled 
+	*/
 
 	// Dynamical system parameters
-	DynSysSchemeData* schemeData = new DynSysSchemeData;
-	schemeData->set_grid(g);
-	schemeData->dynSys = pl;
-	schemeData->uMode = DynSys_UMode_Min;
-	schemeData->dMode = DynSys_DMode_Max;
+	/* 
+	To Be filled 
+	*/
 
 	// Target set and visualization
-	helperOC::HJIPDE_extraArgs extraArgs;
-	helperOC::HJIPDE_extraOuts extraOuts;
-	extraArgs.targets = targets;
-	extraArgs.obstacles = obstacles;
-	extraArgs.stopInit = pl->get_x();
-	extraArgs.visualize = true;
-	extraArgs.plotData.plotDims = beacls::IntegerVec{ 1, 1, 0 };
-	extraArgs.plotData.projpt = beacls::FloatVec{ pl->get_x()[2] };
-	extraArgs.deleteLastPlot = true;
-	if (dump_file) {
-		extraArgs.fig_filename = "MyPlane_test0_BRS";
-	}
+	/* 
+	To Be filled 
+	*/
 
-	HJIPDE* hjipde = new HJIPDE();
+	/* 
+	To Be filled 
+	*/
 
-	beacls::FloatVec tau2;
-	hjipde->solve(tau2, extraOuts, targets, tau, schemeData, HJIPDE::MinWithType_None, extraArgs);
-
-	std::vector<beacls::FloatVec > datas;
-	hjipde->get_datas(datas, tau, schemeData);
-
-	std::string MyPlane_test0_filename("MyPlane_test0.mat");
-	beacls::MatFStream* fs = beacls::openMatFStream(MyPlane_test0_filename, beacls::MatOpenMode_Write);
-	if (dump_file) {
-		beacls::IntegerVec Ns = g->get_Ns();
-
-		g->save_grid(std::string("g"), fs);
-		if (!datas.empty()) save_vector_of_vectors(datas, std::string("data"), Ns, false, fs);
-		if (!tau2.empty()) save_vector(tau2, std::string("tau2"), Ns, false, fs);
-	}
+	/* 
+	To Be filled 
+	*/
 	//!< Compute optimal trajectory
-	extraArgs.projDim = beacls::IntegerVec{ 1,1,0 };
-	if (dump_file) {
-		extraArgs.fig_filename = "MyPlane_test0_Traj";
-	}
-	std::vector<beacls::FloatVec > traj;
-	beacls::FloatVec traj_tau;
-	std::vector<beacls::FloatVec > fliped_data(datas.size());
-	std::copy(datas.crbegin(), datas.crend(), fliped_data.begin());
-	helperOC::ComputeOptTraj* computeOptTraj = new helperOC::ComputeOptTraj();
-	computeOptTraj->operator()(traj, traj_tau, g, fliped_data, tau2, pl, extraArgs);
-	if (computeOptTraj) delete computeOptTraj;
-	if (dump_file) {
-		if (!traj.empty()) save_vector_of_vectors(traj, std::string("traj"), beacls::IntegerVec(), false, fs);
-		if (!traj_tau.empty()) save_vector(traj_tau, std::string("traj_tau"), beacls::IntegerVec(), false, fs);
-	}
-	beacls::closeMatFStream(fs);
+	/* 
+	To Be filled 
+	*/
 
+	/* 
+	To Be filled 
+	*/
 
-	if (hjipde) delete hjipde;
-	if (shapeObs2) delete shapeObs2;
-	if (shapeObs1) delete shapeObs1;
-	if (shapeTarget) delete shapeTarget;
-	if (schemeData) delete schemeData;
-	if (pl) delete pl;
-	if (g) delete g;
+	/* 
+	To Be filled 
+	*/
 	return 0;
 }
 
--- sources/samples/MyPlane_test1/MyPlane.cpp
+++ sources/samples/MyPlane_test1/MyPlane.cpp
@@ -75,51 +75,9 @@ bool MyPlane::optCtrl(
 	const beacls::IntegerVec& deriv_sizes,
 	const DynSys_UMode_Type uMode
 ) const {
-	const DynSys_UMode_Type modified_uMode = (uMode == DynSys_UMode_Default) ? DynSys_UMode_Max : uMode;
-	const FLOAT_TYPE* deriv0_ptr = deriv_ptrs[0];
-	const FLOAT_TYPE* deriv1_ptr = deriv_ptrs[1];
-	const FLOAT_TYPE* deriv2_ptr = deriv_ptrs[2];
-	beacls::FloatVec::const_iterator ys2 = y_ites[2];
-	const size_t y2_size = y_sizes[2];
-	const size_t deriv0_size = deriv_sizes[0];
-	if (y2_size == 0 || deriv0_size == 0 || deriv0_ptr == NULL || deriv1_ptr == NULL || deriv2_ptr == NULL) return false;
-	uOpts.resize(get_nu());
-	uOpts[0].resize(y2_size);
-	uOpts[1].resize(deriv0_size);
-	beacls::FloatVec& uOpt0 = uOpts[0];
-	beacls::FloatVec& uOpt1 = uOpts[1];
-	const auto vrange_minmax = beacls::minmax_value<FLOAT_TYPE>(vrange.cbegin(), vrange.cend());
-	const FLOAT_TYPE vrange_min = vrange_minmax.first;
-	const FLOAT_TYPE vrange_max = vrange_minmax.second;
-
-	if ((modified_uMode == DynSys_UMode_Max) || (modified_uMode == DynSys_UMode_Min)) {
-		const FLOAT_TYPE moded_vrange_max = (modified_uMode == DynSys_UMode_Max) ? vrange_max : vrange_min;
-		const FLOAT_TYPE moded_vrange_min = (modified_uMode == DynSys_UMode_Max) ? vrange_min : vrange_max;
-		const FLOAT_TYPE moded_wMax = (modified_uMode == DynSys_UMode_Max) ? wMax : -wMax;
-		if (deriv0_size != y2_size) {
-			const FLOAT_TYPE deriv0 = deriv0_ptr[0];
-			const FLOAT_TYPE deriv1 = deriv1_ptr[0];
-			std::transform(ys2, ys2 + y2_size, uOpt0.begin(), [deriv0, deriv1, moded_vrange_max, moded_vrange_min](const auto& rhs){
-				const FLOAT_TYPE y2 = rhs;
-				const FLOAT_TYPE det1 = deriv0 * std::cos(y2) + deriv1 * std::sin(y2);
-				return (det1 >= 0) ? moded_vrange_max : moded_vrange_min;
-			});
-		}
-		else {
-			for (size_t index = 0; index < y2_size; ++index) {
-				const FLOAT_TYPE y2 = ys2[index];
-				const FLOAT_TYPE deriv0 = deriv0_ptr[index];
-				const FLOAT_TYPE deriv1 = deriv1_ptr[index];
-				const FLOAT_TYPE det1 = deriv0 * std::cos(y2) + deriv1 * std::sin(y2);
-				uOpt0[index] = (det1 >= 0) ? moded_vrange_max : moded_vrange_min;
-			}
-		}
-		std::transform(deriv2_ptr, deriv2_ptr + deriv0_size, uOpt1.begin(), [moded_wMax](const auto& rhs) { return (rhs >= 0) ? moded_wMax : -moded_wMax;  });
-	}
-	else {
-		std::cerr << "Unknown uMode!: " << uMode << std::endl;
-		return false;
-	}
+	/* 
+	To Be filled 
+	*/
 	return true;
 }
 bool MyPlane::optDstb(
@@ -131,38 +89,9 @@ bool MyPlane::optDstb(
 	const beacls::IntegerVec& deriv_sizes,
 	const DynSys_DMode_Type dMode
 ) const {
-	const DynSys_DMode_Type modified_dMode = (dMode == DynSys_DMode_Default) ? DynSys_DMode_Min : dMode;
-	const FLOAT_TYPE* deriv0_ptr = deriv_ptrs[0];
-	const FLOAT_TYPE* deriv1_ptr = deriv_ptrs[1];
-	const FLOAT_TYPE* deriv2_ptr = deriv_ptrs[2];
-
-	const size_t deriv0_size = deriv_sizes[0];
-	if (deriv0_size == 0 || deriv0_ptr == NULL || deriv1_ptr == NULL || deriv2_ptr == NULL) return false;
-	dOpts.resize(get_nd());
-	std::for_each(dOpts.begin(), dOpts.end(), [deriv0_size](auto& rhs) { rhs.resize(deriv0_size); });
-	beacls::FloatVec& dOpt0 = dOpts[0];
-	beacls::FloatVec& dOpt1 = dOpts[1];
-	beacls::FloatVec& dOpt2 = dOpts[2];
-	const FLOAT_TYPE dMax_0 = dMax[0];
-	const FLOAT_TYPE dMax_1 = dMax[1];
-
-	if ((modified_dMode == DynSys_DMode_Max) || (modified_dMode == DynSys_DMode_Min)) {
-		const FLOAT_TYPE moded_dMax_0 = (modified_dMode == DynSys_DMode_Max) ? dMax_0 : -dMax_0;
-		const FLOAT_TYPE moded_dMax_1 = (modified_dMode == DynSys_DMode_Max) ? dMax_1 : -dMax_1;
-		for (size_t index = 0; index < deriv0_size; ++index) {
-			const FLOAT_TYPE deriv0 = deriv0_ptr[index];
-			const FLOAT_TYPE deriv1 = deriv1_ptr[index];
-			const FLOAT_TYPE deriv2 = deriv2_ptr[index];
-			const FLOAT_TYPE normDeriv01 = std::sqrt(deriv0 * deriv0 + deriv1 * deriv1);
-			dOpt0[index] = (normDeriv01 == 0) ? 0 : moded_dMax_0 * deriv0 / normDeriv01;
-			dOpt1[index] = (normDeriv01 == 0) ? 0 : moded_dMax_0 * deriv1 / normDeriv01;
-			dOpt2[index] = (deriv2 >= 0) ? moded_dMax_1 : -moded_dMax_1;
-		}
-	}
-	else {
-		std::cerr << "Unknown dMode!: " << modified_dMode << std::endl;
-		return false;
-	}
+	/* 
+	To Be filled 
+	*/
 	return true;
 }
 
@@ -181,49 +110,23 @@ bool MyPlane::dynamics_cell_helper(
 	switch (dim) {
 	case 0:
 		{
-			const beacls::FloatVec& ds_0 = ds[0];
-			const beacls::FloatVec& us_0 = us[0];
-			if (ds[0].size() == dx_dim_size) {
-				for (size_t index = 0; index < dx_dim_size; ++index) {
-					dx_dim[index] = us_0[index] * std::cos(x_ite[index]) + ds_0[index];
-				}
-			}
-			else {	//!< ds_0_size != dx_dim_size
-				const FLOAT_TYPE d0 = ds_0[0];
-				std::transform(x_ite, x_ite + dx_dim_size, us_0.cbegin(), dx_dim.begin(), [d0](const auto& lhs, const auto& rhs) {
-					return  rhs * std::cos(lhs) + d0;
-				});
-			}
+			/* 
+			To Be filled 
+			*/
 		}
 		break;
 	case 1:
 		{
-			const beacls::FloatVec& ds_1 = ds[1];
-			const beacls::FloatVec& us_0 = us[0];
-			if (ds[1].size() == dx_dim_size) {
-				for (size_t index = 0; index < dx_dim_size; ++index) {
-					dx_dim[index] = us_0[index] * std::sin(x_ite[index]) + ds_1[index];
-				}
-			}
-			else {
-				const FLOAT_TYPE d1 = ds_1[0];
-				std::transform(x_ite, x_ite + dx_dim_size, us_0.cbegin(), dx_dim.begin(), [d1](const auto& lhs, const auto& rhs) {
-					return  rhs * std::sin(lhs) + d1;
-				});
-			}
+			/* 
+			To Be filled 
+			*/
 		}
 		break;
 	case 2:
 		{
-			const beacls::FloatVec& ds_2 = ds[2];
-			const beacls::FloatVec& us_1 = us[1];
-			if (ds[2].size() == dx_dim_size) {
-				std::transform(us_1.cbegin(), us_1.cbegin() + dx_dim_size, ds_2.cbegin(), dx_dim.begin(), std::plus<FLOAT_TYPE>());
-			}
-			else {
-				std::cerr << __FILE__ << ":" << __LINE__ << ":" << __func__ << " Invalid data size" << std::endl;
-				result = false;
-			}
+			/* 
+			To Be filled 
+			*/
 		}
 		break;
 	default:
