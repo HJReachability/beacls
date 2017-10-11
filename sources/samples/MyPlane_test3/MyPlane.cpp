e380a17d2a8e4e9a453611c15d434528a33245ee
Ken Tanabe <k-tanabe@an.wakwak.com>
2017-10-11 23:09:21 +0900

    Modified after build commands, copy dependent dlls to project directory. Added a new test

---
+++ sources/samples/MyPlane_test3/MyPlane.cpp
@@ -0,0 +1,286 @@
+#include <helperOC/helperOC.hpp>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include "MyPlane_cuda.hpp"
+#include "MyPlane.hpp"
+MyPlane::MyPlane(
+	const beacls::FloatVec &x,
+	const FLOAT_TYPE wMax,
+	const beacls::FloatVec& vrange,
+	const beacls::FloatVec& dMax
+) : DynSys(3, 2, 3,
+	beacls::IntegerVec{0, 1},	//!< Position dimensions
+	beacls::IntegerVec{2}),	//!< velocity dimensions
+	wMax(wMax), vrange(vrange), dMax(dMax) {
+	if (x.size() != 3) {
+		std::cerr << "Error: " << __func__ << " : Initial state does not have right dimension!" << std::endl;
+	}
+
+	DynSys::set_x(x);
+	DynSys::push_back_xhist(x);
+
+}
+MyPlane::~MyPlane() {
+}
+bool MyPlane::optCtrl(
+	std::vector<beacls::FloatVec >& uOpts,
+	const FLOAT_TYPE,
+	const std::vector<beacls::FloatVec::const_iterator >& y_ites,
+	const std::vector<const FLOAT_TYPE*>& deriv_ptrs,
+	const beacls::IntegerVec& y_sizes,
+	const beacls::IntegerVec& deriv_sizes,
+	const helperOC::DynSys_UMode_Type uMode
+) const {
+	const helperOC::DynSys_UMode_Type modified_uMode = (uMode == helperOC::DynSys_UMode_Default) ? helperOC::DynSys_UMode_Max : uMode;
+	const FLOAT_TYPE* deriv0_ptr = deriv_ptrs[0];
+	const FLOAT_TYPE* deriv1_ptr = deriv_ptrs[1];
+	const FLOAT_TYPE* deriv2_ptr = deriv_ptrs[2];
+	beacls::FloatVec::const_iterator ys2 = y_ites[2];
+	const size_t y2_size = y_sizes[2];
+	const size_t deriv0_size = deriv_sizes[0];
+	if (y2_size == 0 || deriv0_size == 0 || deriv0_ptr == NULL || deriv1_ptr == NULL || deriv2_ptr == NULL) return false;
+	uOpts.resize(get_nu());
+	uOpts[0].resize(y2_size);
+	uOpts[1].resize(deriv0_size);
+	beacls::FloatVec& uOpt0 = uOpts[0];
+	beacls::FloatVec& uOpt1 = uOpts[1];
+	const auto vrange_minmax = beacls::minmax_value<FLOAT_TYPE>(vrange.cbegin(), vrange.cend());
+	const FLOAT_TYPE vrange_min = vrange_minmax.first;
+	const FLOAT_TYPE vrange_max = vrange_minmax.second;
+
+	if ((modified_uMode == helperOC::DynSys_UMode_Max) || (modified_uMode == helperOC::DynSys_UMode_Min)) {
+		const FLOAT_TYPE moded_vrange_max = (modified_uMode == helperOC::DynSys_UMode_Max) ? vrange_max : vrange_min;
+		const FLOAT_TYPE moded_vrange_min = (modified_uMode == helperOC::DynSys_UMode_Max) ? vrange_min : vrange_max;
+		const FLOAT_TYPE moded_wMax = (modified_uMode == helperOC::DynSys_UMode_Max) ? wMax : -wMax;
+		if (deriv0_size != y2_size) {
+			const FLOAT_TYPE deriv0 = deriv0_ptr[0];
+			const FLOAT_TYPE deriv1 = deriv1_ptr[0];
+			std::transform(ys2, ys2 + y2_size, uOpt0.begin(), [deriv0, deriv1, moded_vrange_max, moded_vrange_min](const auto& rhs){
+				const FLOAT_TYPE y2 = rhs;
+				const FLOAT_TYPE det1 = deriv0 * std::cos(y2) + deriv1 * std::sin(y2);
+				return (det1 >= 0) ? moded_vrange_max : moded_vrange_min;
+			});
+		}
+		else {
+			for (size_t index = 0; index < y2_size; ++index) {
+				const FLOAT_TYPE y2 = ys2[index];
+				const FLOAT_TYPE deriv0 = deriv0_ptr[index];
+				const FLOAT_TYPE deriv1 = deriv1_ptr[index];
+				const FLOAT_TYPE det1 = deriv0 * std::cos(y2) + deriv1 * std::sin(y2);
+				uOpt0[index] = (det1 >= 0) ? moded_vrange_max : moded_vrange_min;
+			}
+		}
+		std::transform(deriv2_ptr, deriv2_ptr + deriv0_size, uOpt1.begin(), [moded_wMax](const auto& rhs) { return (rhs >= 0) ? moded_wMax : -moded_wMax;  });
+	}
+	else {
+		std::cerr << "Unknown uMode!: " << uMode << std::endl;
+		return false;
+	}
+	return true;
+}
+bool MyPlane::optDstb(
+	std::vector<beacls::FloatVec >& dOpts,
+	const FLOAT_TYPE,
+	const std::vector<beacls::FloatVec::const_iterator >&,
+	const std::vector<const FLOAT_TYPE*>& deriv_ptrs,
+	const beacls::IntegerVec&,
+	const beacls::IntegerVec& deriv_sizes,
+	const helperOC::DynSys_DMode_Type dMode
+) const {
+	const helperOC::DynSys_DMode_Type modified_dMode = (dMode == helperOC::DynSys_DMode_Default) ? helperOC::DynSys_DMode_Min : dMode;
+	const FLOAT_TYPE* deriv0_ptr = deriv_ptrs[0];
+	const FLOAT_TYPE* deriv1_ptr = deriv_ptrs[1];
+	const FLOAT_TYPE* deriv2_ptr = deriv_ptrs[2];
+
+	const size_t deriv0_size = deriv_sizes[0];
+	if (deriv0_size == 0 || deriv0_ptr == NULL || deriv1_ptr == NULL || deriv2_ptr == NULL) return false;
+	dOpts.resize(get_nd());
+	std::for_each(dOpts.begin(), dOpts.end(), [deriv0_size](auto& rhs) { rhs.resize(deriv0_size); });
+	beacls::FloatVec& dOpt0 = dOpts[0];
+	beacls::FloatVec& dOpt1 = dOpts[1];
+	beacls::FloatVec& dOpt2 = dOpts[2];
+	const FLOAT_TYPE dMax_0 = dMax[0];
+	const FLOAT_TYPE dMax_1 = dMax[1];
+
+	if ((modified_dMode == helperOC::DynSys_DMode_Max) || (modified_dMode == helperOC::DynSys_DMode_Min)) {
+		const FLOAT_TYPE moded_dMax_0 = (modified_dMode == helperOC::DynSys_DMode_Max) ? dMax_0 : -dMax_0;
+		const FLOAT_TYPE moded_dMax_1 = (modified_dMode == helperOC::DynSys_DMode_Max) ? dMax_1 : -dMax_1;
+		for (size_t index = 0; index < deriv0_size; ++index) {
+			const FLOAT_TYPE deriv0 = deriv0_ptr[index];
+			const FLOAT_TYPE deriv1 = deriv1_ptr[index];
+			const FLOAT_TYPE deriv2 = deriv2_ptr[index];
+			const FLOAT_TYPE normDeriv01 = std::sqrt(deriv0 * deriv0 + deriv1 * deriv1);
+			dOpt0[index] = (normDeriv01 == 0) ? 0 : moded_dMax_0 * deriv0 / normDeriv01;
+			dOpt1[index] = (normDeriv01 == 0) ? 0 : moded_dMax_0 * deriv1 / normDeriv01;
+			dOpt2[index] = (deriv2 >= 0) ? moded_dMax_1 : -moded_dMax_1;
+		}
+	}
+	else {
+		std::cerr << "Unknown dMode!: " << modified_dMode << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool MyPlane::dynamics_cell_helper(
+	beacls::FloatVec& dx,
+	const beacls::FloatVec::const_iterator& x_ite,
+	const std::vector<beacls::FloatVec >& us,
+	const std::vector<beacls::FloatVec >& ds,
+	const size_t x_size,
+	const size_t dim
+) const {
+	beacls::FloatVec& dx_dim = dx;
+	const size_t dx_dim_size = (dim == 2) ? us[1].size() : x_size;
+	dx.resize(dx_dim_size);
+	bool result = true;
+	switch (dim) {
+	case 0:
+		{
+			const beacls::FloatVec& ds_0 = ds[0];
+			const beacls::FloatVec& us_0 = us[0];
+			if (ds[0].size() == dx_dim_size) {
+				for (size_t index = 0; index < dx_dim_size; ++index) {
+					dx_dim[index] = us_0[index] * std::cos(x_ite[index]) + ds_0[index];
+				}
+			}
+			else {	//!< ds_0_size != dx_dim_size
+				const FLOAT_TYPE d0 = ds_0[0];
+				std::transform(x_ite, x_ite + dx_dim_size, us_0.cbegin(), dx_dim.begin(), [d0](const auto& lhs, const auto& rhs) {
+					return  rhs * std::cos(lhs) + d0;
+				});
+			}
+		}
+		break;
+	case 1:
+		{
+			const beacls::FloatVec& ds_1 = ds[1];
+			const beacls::FloatVec& us_0 = us[0];
+			if (ds[1].size() == dx_dim_size) {
+				for (size_t index = 0; index < dx_dim_size; ++index) {
+					dx_dim[index] = us_0[index] * std::sin(x_ite[index]) + ds_1[index];
+				}
+			}
+			else {
+				const FLOAT_TYPE d1 = ds_1[0];
+				std::transform(x_ite, x_ite + dx_dim_size, us_0.cbegin(), dx_dim.begin(), [d1](const auto& lhs, const auto& rhs) {
+					return  rhs * std::sin(lhs) + d1;
+				});
+			}
+		}
+		break;
+	case 2:
+		{
+			const beacls::FloatVec& ds_2 = ds[2];
+			const beacls::FloatVec& us_1 = us[1];
+			if (ds[2].size() == dx_dim_size) {
+				std::transform(us_1.cbegin(), us_1.cbegin() + dx_dim_size, ds_2.cbegin(), dx_dim.begin(), std::plus<FLOAT_TYPE>());
+			}
+			else {
+				std::cerr << __FILE__ << ":" << __LINE__ << ":" << __func__ << " Invalid data size" << std::endl;
+				result = false;
+			}
+		}
+		break;
+	default:
+		std::cerr << "Only dimension 0-2 are defined for dynamics of MyPlane!" << std::endl;
+		result = false;
+		break;
+	}
+	return result;
+}
+
+bool MyPlane::dynamics(
+	std::vector<beacls::FloatVec >& dxs,
+	const FLOAT_TYPE,
+	const std::vector<beacls::FloatVec::const_iterator >& x_ites,
+	const std::vector<beacls::FloatVec >& us,
+	const std::vector<beacls::FloatVec >& ds,
+	const beacls::IntegerVec& x_sizes,
+	const size_t dst_target_dim
+) const {
+	static const std::vector<beacls::FloatVec >& dummy_ds{ beacls::FloatVec{0},beacls::FloatVec{0},beacls::FloatVec{0} };
+	const std::vector<beacls::FloatVec >& modified_ds = (ds.empty()) ? dummy_ds : ds;
+	const size_t src_x_dim_index = 2;
+	const beacls::FloatVec::const_iterator& x_ites_target_dim = x_ites[src_x_dim_index];
+	if (dst_target_dim == std::numeric_limits<size_t>::max()) {
+		dynamics_cell_helper(dxs[0], x_ites_target_dim, us, modified_ds, x_sizes[src_x_dim_index], 0);
+		dynamics_cell_helper(dxs[1], x_ites_target_dim, us, modified_ds, x_sizes[src_x_dim_index], 1);
+		dynamics_cell_helper(dxs[2], x_ites_target_dim, us, modified_ds, x_sizes[src_x_dim_index], 2);
+	}
+	else
+	{
+		if (dst_target_dim < x_ites.size())
+			dynamics_cell_helper(dxs[dst_target_dim], x_ites_target_dim, us, modified_ds, x_sizes[src_x_dim_index], dst_target_dim);
+		else
+			std::cerr << "Invalid target dimension for dynamics: " << dst_target_dim << std::endl;
+	}
+	return true;
+}
+
+#if defined(WIGH_GPU)
+bool MyPlane::optCtrl_cuda(
+	std::vector<beacls::UVec>& u_uvecs,
+	const FLOAT_TYPE,
+	const std::vector<beacls::UVec>& x_uvecs,
+	const std::vector<beacls::UVec>& deriv_uvecs,
+	const DynSys_UMode_Type uMode
+) const {
+	if (x_uvecs.size() < 3 || x_uvecs[2].empty() || deriv_uvecs.size() < 3 || deriv_uvecs[0].empty() || deriv_uvecs[1].empty() || deriv_uvecs[2].empty()) return false;
+	const DynSys_UMode_Type modified_uMode = (uMode == helperOC::DynSys_UMode_Default) ? helperOC::DynSys_UMode_Max : uMode;
+	const auto vrange_minmax = beacls::minmax_value<FLOAT_TYPE>(vrange.cbegin(), vrange.cend());
+	const FLOAT_TYPE vrange_min = vrange_minmax.first;
+	const FLOAT_TYPE vrange_max = vrange_minmax.second;
+	return MyPlane_CUDA::optCtrl_execute_cuda(u_uvecs, x_uvecs, deriv_uvecs, wMax, vrange_max, vrange_min, modified_uMode);
+}
+bool MyPlane::optDstb_cuda(
+	std::vector<beacls::UVec>& d_uvecs,
+	const FLOAT_TYPE,
+	const std::vector<beacls::UVec>& x_uvecs,
+	const std::vector<beacls::UVec>& deriv_uvecs,
+	const DynSys_DMode_Type dMode
+) const {
+	if (deriv_uvecs.size() < 3 || deriv_uvecs[0].empty() || deriv_uvecs[1].empty() || deriv_uvecs[2].empty()) return false;
+	const DynSys_DMode_Type modified_dMode = (dMode == helperOC::DynSys_DMode_Default) ? helperOC::DynSys_DMode_Min : dMode;
+	return MyPlane_CUDA::optDstb_execute_cuda(d_uvecs, x_uvecs, deriv_uvecs, dMax, modified_dMode);
+}
+bool MyPlane::dynamics_cuda(
+	std::vector<beacls::UVec>& dx_uvecs,
+	const FLOAT_TYPE,
+	const std::vector<beacls::UVec>& x_uvecs,
+	const std::vector<beacls::UVec>& u_uvecs,
+	const std::vector<beacls::UVec>& d_uvecs,
+	const size_t dst_target_dim
+) const {
+	beacls::FloatVec dummy_d_vec{ 0 };
+	std::vector<beacls::UVec> dummy_d_uvecs;
+	if (d_uvecs.empty()) {
+		dummy_d_uvecs.resize(get_nd());
+		std::for_each(dummy_d_uvecs.begin(), dummy_d_uvecs.end(), [&dummy_d_vec](auto& rhs) {
+			rhs = beacls::UVec(dummy_d_vec, beacls::UVecType_Vector, false);
+		});
+	}
+	const std::vector<beacls::UVec>& modified_d_uvecs = (d_uvecs.empty()) ? dummy_d_uvecs : d_uvecs;
+	bool result = true;
+	if (dst_target_dim == std::numeric_limits<size_t>::max()) {
+		result &= MyPlane_CUDA::dynamics_cell_helper_execute_cuda(dx_uvecs[0], x_uvecs, u_uvecs, modified_d_uvecs, 0);
+		result &= MyPlane_CUDA::dynamics_cell_helper_execute_cuda(dx_uvecs[1], x_uvecs, u_uvecs, modified_d_uvecs, 1);
+		result &= MyPlane_CUDA::dynamics_cell_helper_execute_cuda(dx_uvecs[2], x_uvecs, u_uvecs, modified_d_uvecs, 2);
+	}
+	else
+	{
+		if (dst_target_dim < x_uvecs.size()) {
+			MyPlane_CUDA::dynamics_cell_helper_execute_cuda(dx_uvecs[dst_target_dim], x_uvecs, u_uvecs, modified_d_uvecs, dst_target_dim);
+		}
+		else {
+			std::cerr << "Invalid target dimension for dynamics: " << dst_target_dim << std::endl;
+			result = false;
+		}
+	}
+	return result;
+}
+
+#endif /* defined(WIGH_GPU) */
---
+++ sources/samples/MyPlane_test3/example/MyPlane_test3.cpp
@@ -0,0 +1,113 @@
+#define _USE_MATH_DEFINES
+#include <levelset/levelset.hpp>
+#include <helperOC/helperOC.hpp>
+#include <cmath>
+#include <numeric>
+#include <functional>
+#include <cfloat>
+#include <cstring>
+#include "MyPlane.hpp"
+
+/**
+	@brief Tests the Plane class by computing a reachable set and then computing the optimal trajectory from the reachable set.
+	*/
+int main(int argc, char *argv[])
+{
+	bool dump_file = false;
+	if (argc >= 2) {
+		dump_file = (atoi(argv[1]) == 0) ? false : true;
+	}
+
+	//!< Plane parameters
+	const beacls::FloatVec initState{ 100, 75, (FLOAT_TYPE)(220 * M_PI / 180) };
+	const FLOAT_TYPE wMax = (FLOAT_TYPE)1.2;
+	const beacls::FloatVec vrange{ (FLOAT_TYPE)1.1, (FLOAT_TYPE)1.3 };
+	const beacls::FloatVec dMax{ 0, 0 };
+	MyPlane* pl = new MyPlane(initState, wMax, vrange, dMax);
+
+	const FLOAT_TYPE inf = std::numeric_limits<FLOAT_TYPE>::infinity();
+	//!< Target and obstacle
+	levelset::HJI_Grid* g = helperOC::createGrid(
+		beacls::FloatVec{0, 0, 0}, 
+		beacls::FloatVec{150, 150, (FLOAT_TYPE)(2*M_PI)},
+		beacls::IntegerVec{41,41,11});
+	std::vector<beacls::FloatVec > targets(1);
+	levelset::ShapeCylinder(beacls::IntegerVec{ 2 }, beacls::FloatVec{ 75, 50, 0 }, 10).execute(g, targets[0]);
+	beacls::FloatVec obs1, obs2;
+	levelset::ShapeRectangleByCorner(beacls::FloatVec{ 300, 250, -inf }, beacls::FloatVec{ 350, 300, inf }).execute(g, obs1);
+	levelset::ShapeRectangleByCorner(beacls::FloatVec{ 5, 5, -inf }, beacls::FloatVec{ 145, 145, inf }).execute(g, obs2);
+	std::transform(obs2.cbegin(), obs2.cend(), obs2.begin(), std::negate<FLOAT_TYPE>());
+	std::vector<beacls::FloatVec > obstacles(1);
+	obstacles[0].resize(obs1.size());
+	std::transform(obs1.cbegin(), obs1.cend(), obs2.cbegin(), obstacles[0].begin(),
+		std::ptr_fun<const FLOAT_TYPE&, const FLOAT_TYPE&>(std::min<FLOAT_TYPE>));
+
+	//!< Compute reachable set
+	const FLOAT_TYPE tMax = 500;
+	const FLOAT_TYPE dt = 0.25;
+	beacls::FloatVec tau = generateArithmeticSequence<FLOAT_TYPE>(0., dt, tMax);
+
+	// Dynamical system parameters
+	helperOC::DynSysSchemeData* schemeData = new helperOC::DynSysSchemeData;
+	schemeData->set_grid(g);
+	schemeData->dynSys = pl;
+	schemeData->uMode = helperOC::DynSys_UMode_Min;
+	schemeData->dMode = helperOC::DynSys_DMode_Max;
+
+	// Target set and visualization
+	helperOC::HJIPDE_extraArgs extraArgs;
+	helperOC::HJIPDE_extraOuts extraOuts;
+	extraArgs.targets = targets;
+	extraArgs.obstacles = obstacles;
+	extraArgs.stopInit = pl->get_x();
+	extraArgs.visualize = true;
+	extraArgs.plotData.plotDims = beacls::IntegerVec{ 1, 1, 0 };
+	extraArgs.plotData.projpt = beacls::FloatVec{ pl->get_x()[2] };
+	extraArgs.deleteLastPlot = true;
+	if (dump_file) {
+		extraArgs.fig_filename = "MyPlane_test3_BRS";
+	}
+	
+	extraArgs.execParameters.useCuda = true;
+
+	helperOC::HJIPDE* hjipde = new helperOC::HJIPDE();
+
+	beacls::FloatVec tau2;
+	std::vector<beacls::FloatVec > datas;
+	hjipde->solve(datas, tau2, extraOuts, targets, tau, schemeData, helperOC::HJIPDE::MinWithType_None, extraArgs);
+
+	std::string MyPlane_test3_filename("MyPlane_test3.mat");
+	beacls::MatFStream* fs = beacls::openMatFStream(MyPlane_test3_filename, beacls::MatOpenMode_Write);
+	if (dump_file) {
+		beacls::IntegerVec Ns = g->get_Ns();
+
+		g->save_grid(std::string("g"), fs);
+		if (!datas.empty()) save_vector_of_vectors(datas, std::string("data"), Ns, false, fs);
+		if (!tau2.empty()) save_vector(tau2, std::string("tau2"), Ns, false, fs);
+	}
+	//!< Compute optimal trajectory
+	extraArgs.projDim = beacls::IntegerVec{ 1,1,0 };
+	if (dump_file) {
+		extraArgs.fig_filename = "MyPlane_test3_Traj";
+	}
+	std::vector<beacls::FloatVec > traj;
+	beacls::FloatVec traj_tau;
+	std::vector<beacls::FloatVec > fliped_data(datas.size());
+	std::copy(datas.crbegin(), datas.crend(), fliped_data.begin());
+	helperOC::ComputeOptTraj* computeOptTraj = new helperOC::ComputeOptTraj();
+	computeOptTraj->operator()(traj, traj_tau, g, fliped_data, tau2, pl, extraArgs);
+	if (computeOptTraj) delete computeOptTraj;
+	if (dump_file) {
+		if (!traj.empty()) save_vector_of_vectors(traj, std::string("traj"), beacls::IntegerVec(), false, fs);
+		if (!traj_tau.empty()) save_vector(traj_tau, std::string("traj_tau"), beacls::IntegerVec(), false, fs);
+	}
+	beacls::closeMatFStream(fs);
+
+
+	if (hjipde) delete hjipde;
+	if (schemeData) delete schemeData;
+	if (pl) delete pl;
+	if (g) delete g;
+	return 0;
+}
+
