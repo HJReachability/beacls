24b4e735dd7b8e94f44b3193d439004aabaa4cef
Ken Tanabe <k-tanabe@an.wakwak.com>
2018-04-05 00:47:36 +0900

    Update to OpenCV 3.4.1 Supported Visual Studio 2017 (vc15)

---
+++ sources/run_visualstudio15_beacls_cuda8.0.bat
@@ -0,0 +1,30 @@
+@echo off
+set HDF5_DIR_X64=C:\Program Files\HDF_Group\HDF5\1.8.18
+set HDF5_DIR_X86=C:\Program Files (x86)\HDF_Group\HDF5\1.8.18
+set MATIO_DIR=%~dp03rdparty\matio\visual_studio
+set BOOST_DIR=C:\Boost\boost_1_63_0
+set OPENCV_DIR=C:\OpenCV3\opencv3.4.1\opencv\build
+set OPENCV_VER=341
+set OPENCV_VC_TOOLSET=vc15
+set OPENCV_DEBUG_LIBS=opencv_world%OPENCV_VER%d.lib
+set OPENCV_RELEASE_LIBS=opencv_world%OPENCV_VER%.lib
+set OPENCV_DEBUG_BINS=opencv_world%OPENCV_VER%d.dll
+set OPENCV_RELEASE_BINS=opencv_world%OPENCV_VER%.dll
+set CUDA_PATH=$(CUDA_PATH_V8_0)
+set CUDA_LIBS=cudart.lib
+set CUDA_MACRO=WITH_GPU
+set SOLUTION_FILE=beacls.sln
+
+if exsit C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe (
+  set VS_EDITION=Enterprise
+) else (
+  if exsit C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe (
+    set VS_EDITION=Professional
+  ) else (
+    set VS_EDITION=Community
+  )
+)
+echo Opening "%SOLUTION_FILE%" on Visua Studio 2017 %VS_EDITION%
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\%VS_EDITION%\Common7\IDE\devenv.exe" "%SOLUTION_FILE%"
+
+timeout 5
---
+++ sources/run_visualstudio15_matio.bat
@@ -0,0 +1,18 @@
+@echo off
+set HDF5_DIR_X64=C:\Program Files\HDF_Group\HDF5\1.8.18
+set HDF5_DIR_X86=C:\Program Files (x86)\HDF_Group\HDF5\1.8.18
+set SOLUTION_FILE=3rdparty\matio\visual_studio\matio.sln
+
+if exsit C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe (
+  set VS_EDITION=Enterprise
+) else (
+  if exsit C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe (
+    set VS_EDITION=Professional
+  ) else (
+    set VS_EDITION=Community
+  )
+)
+echo Opening "%SOLUTION_FILE%" on Visua Studio 2017 %VS_EDITION%
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\%VS_EDITION%\Common7\IDE\devenv.exe" "%SOLUTION_FILE%"
+
+timeout 5
