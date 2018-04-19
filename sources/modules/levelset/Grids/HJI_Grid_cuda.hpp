a0c0aa15e681bd0accffd783bd1d37d0f6537744
Ken Tanabe <k-tanabe@an.wakwak.com>
2018-04-19 09:51:10 +0900

    Under implementation.

--- sources/modules/levelset/Grids/HJI_Grid_cuda.hpp
+++ sources/modules/levelset/Grids/HJI_Grid_cuda.hpp
@@ -10,6 +10,7 @@ void HJI_Grid_calc_xs_execute_cuda
 	const size_t dimension,
 	const size_t start_index,
 	const size_t loop_length,
-	const size_t stride
+	const size_t stride,
+	const std::vector<size_t>& Ns
 );
 #endif	/* __HJI_Grid_cuda_hpp__ */
